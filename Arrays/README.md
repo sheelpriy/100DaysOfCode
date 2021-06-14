6b712e741a7f62ee75b89119589046e74294768d
sheel <sheel@clik.ai>
2021-06-14 21:27:52 +0530

    find max consecutive 1s in array of binaries

---
+++ Arrays/README.md
@@ -0,0 +1,27 @@
+## What is an Array
+An Array is a collection of items. The items could be integers, strings, DVDs, games, books—anything really. The items are stored in neighboring (contiguous) memory locations. Because they're stored together, checking through the entire collection of items is straightforward.
+
+
+## Creating an Array
+`
+creating an array of defined length with Array constructor
+    const arr =  new Array(5);
+
+creating an array with square brakcets
+    const arr = [];
+`
+
+## Writing Items into an Array
+`
+writing to a given index index
+    arr[index] = value
+
+writing at the end of array
+    arr.push(value);
+`
+
+## Reasding items from Array
+`
+read value from a given index
+    const value = arr[index]
+`
---
+++ Arrays/maxConsicutiveOne.js
@@ -0,0 +1,33 @@
+/* 
+Q. Given a binary array nums, return the maximum number of consecutive 1's in the array.
+
+
+Input: nums = [1,1,0,1,1,1]
+Output: 3
+Explanation: The first two digits or the last three digits are consecutive 1s. The maximum number of consecutive 1s is 3.
+
+*/
+
+var findMaxConsecutiveOnes = function(nums) {
+    let maxCount=0;
+    let currentCount = 0;
+    for(let i=0; i<nums.length; i++){
+        if(nums[i]===1){
+            currentCount++;
+             if(currentCount>maxCount){
+                 maxCount = currentCount;
+             }
+        }else{
+            currentCount =0;
+        }
+    }
+    if(currentCount>maxCount){
+        maxCount = currentCount;
+    }
+    
+    
+    return maxCount;
+};
+
+
+findMaxConsecutiveOnes([1,1,0,1,1,1]) // should output 3
