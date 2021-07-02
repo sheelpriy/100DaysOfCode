d9204c963b74be4a1524a3c00033130fdde0677e
sheel <sheel@clik.ai>
2021-07-02 13:18:48 +0530

    singly linked list implementation in JS

---
+++ LinkedList/createAndPrint.js
@@ -0,0 +1,13 @@
+const LinkedList = require("./linkedinList")
+
+let lList = new LinkedList()
+lList.add(2);
+lList.add(3);
+lList.add(4);
+lList.add(7);
+lList.add(9);
+lList.add(11);
+lList.add(12);
+
+
+console.log(lList)
--- LinkedList/linkedinList.js
+++ LinkedList/linkedinList.js
@@ -1,3 +1,41 @@
 /* 
 linkedin implementation in JS
 */
+
+class Node {
+    constructor(data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+class LinkedList {
+    constructor() {
+        this.head = null;
+        this.size = 0;
+    }
+    // adds an element at the end of list
+    add(element) {
+        // creates a new node
+        let node = new Node(element);
+        // to store current node
+        let current;
+        // if list is Empty add the
+        // element and make it head
+        if (this.head == null){
+            this.head = node;
+        }else {
+            current = this.head;
+            // find the last node
+            while (current.next) {
+                current = current.next;
+            }
+            current.next = node;
+        }
+        this.size++;
+    }
+}
+
+
+
+module.exports = LinkedList;
